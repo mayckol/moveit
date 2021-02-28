29dcf82a1cac054bc45b672b02d842983c5f71da
mayckol <mayckoll@live.com>
2021-02-28 19:38:42 -0300

    finaliza estilizacao
    - exemplo de brightness, escurecer o botao usando o hover

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/pages/Home.module.css'
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import Head from "next/head";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
           <Countdown/>
         </div>
         <div>
+          <ChallengeBox/>
         </div>
       </section>
     </div>
